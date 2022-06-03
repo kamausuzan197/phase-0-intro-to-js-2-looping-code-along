c9dea4f3383d2b3e0a6e6babbcc85c738664e66e
kamausuzan197 <kamaususan197@gmail.com>
2022-06-03 07:50:18 +0300

    learning loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let cards = ['Lisa', 'Kaitlin', 'Jan'];
+  function writeCards(cards) {
+    let array = [];
+    for (let i = 0; i < cards.length; i++) {
+    array.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+   debugger; 
+  } 
+   return array;
+}
+
+function countDown() {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i - 0);
+    i--;
+  }
+}
+countDown(10);
